5b41bc16f4d832089f862ae2c3ab9106de28c4db
Tyler Betzler <Tyler.J.Betzler@gmail.com>
2018-07-19 20:01:39 -0500

    Using models.

--- LocationTracker/Data/TrackerContext.cs
+++ LocationTracker/Data/TrackerContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using LocationTracker.Models;
 
 namespace LocationTracker.Data
 {
