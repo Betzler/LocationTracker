b999f4fd88ab69423b9834faa1cb6dc8dae4ecd4
Tyler Betzler <Tyler.J.Betzler@gmail.com>
2018-07-19 19:49:09 -0500

    Initial changes to startup and program.

--- LocationTracker/Program.cs
+++ LocationTracker/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using LocationTracker.Models;
+using Microsoft.Extensions.DependencyInjection;
+using LocationTracker.Data;
 
 namespace LocationTracker
 {
@@ -14,7 +17,25 @@ namespace LocationTracker
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<TrackerContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
